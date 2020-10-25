ad02c5391f67ea05170accb7f0b765ea833953fd
ace2267 <ace22672@gmail.com>
2020-10-25 17:45:48 +0900

    Add files via upload

---
+++ cryptoPjt/src/main/java/com/crypto/study/CryptoPjtApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPjtApplication.class, args);
+	}
+
+}
---
+++ cryptoPjt/src/test/java/com/crypto/study/CryptoPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
